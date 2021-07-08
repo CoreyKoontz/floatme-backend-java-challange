1ade8e99b536185dae10ef60007ddb549fde46f3
Corey Koontz <corey.michael.koontz@gmail.com>
2021-07-08 15:28:18 -0500

    Feat: Jobs API for returning JSON

--- src/main/java/com/floatme/floatme/controllers/JobController.java
+++ src/main/java/com/floatme/floatme/controllers/JobController.java
@@ -1,7 +1,12 @@
 package com.floatme.floatme.controllers;
 
+import com.floatme.floatme.models.Job;
 import com.floatme.floatme.repositories.JobRepo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class JobController {
@@ -10,5 +15,15 @@ public class JobController {
 
     public JobController(JobRepo jobDao) {
         this.jobDao = jobDao;
+    }
+
+    @GetMapping("/jobs.json")
+    public @ResponseBody List<Job> viewAllJobsInJSONFormat() {
+        return jobDao.findAll();
+    }
+
+    @GetMapping("/jobs/ajax")
+    public String viewAllJobsWithAjax(){
+        return "/people/home";
     }
 }
