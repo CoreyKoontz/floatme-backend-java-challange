3a9da4c969e3ec01dd178b512c4e68c57a9e3d83
Corey Koontz <corey.michael.koontz@gmail.com>
2021-07-07 22:42:16 -0500

    Adding user seeder file

---
+++ seeder.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS floatme_db;
+
+USE floatme_db;
+
+INSERT INTO users (name, age, date_joined, date_updated) VALUES
+('Jim', 30, '2015-05-25', '2019-12-14'),
+('Mike', 45, '2008-03-22', '2020-08-20'),
+('Pam', 30, '2012-07-15', '2021-03-11');
